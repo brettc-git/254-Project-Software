9022fd030b7380a317c3f55155c2582293d21665
Brett Chiu <72076153+brettc-git@users.noreply.github.com>
2024-11-14 00:33:42 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <wx/wx.h>
+
+class App : public wxApp {
+public:
+	virtual bool OnInit();
+};
+
+class Frame : public wxFrame {
+public:
+	Frame(const wxString& title);
+private:
+	void OnAbout(wxCommandEvent& event);
+	void OnExit(wxCommandEvent& event);
+};
+
+bool App::OnInit()
+{
+	wxFrame* window = new wxFrame(NULL, wxID_ANY, "Password Strength Checker", wxDefaultPosition, wxDefaultSize);
+	window->Show();
+	return true;
+}
+
+Frame::Frame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title)
+{
+
+	// wxCheckBox* showPasswordCheckbox = new wxCheckBox(, wxID_ANY, "Show Password")
+	wxMenu* menu = new wxMenu;
+	
+	// Menu bar implementation 
+	wxMenu* menuFile = new wxMenu;
+	wxMenu* menuEdit = new wxMenu;
+	wxMenu* menuHelp = new wxMenu;
+	wxMenuBar* menuBar = new wxMenuBar;
+	menuBar->Append(menuFile, "&File");
+	menuBar->Append(menuEdit, "&Edit");
+	menuBar->Append(menuHelp, "&Help");
+
+	SetMenuBar(menuBar);
+
+}
+
+void Frame::OnAbout(wxCommandEvent& event) {
+	wxMessageBox("Team Members: Brett Chiu, Rene Acosta, ")
+}
+// Exit function 
+void Frame::OnExit(wxCommandEvent& event) {
+	Close(true);
+}
+wxIMPLEMENT_APP(App);
+
+
