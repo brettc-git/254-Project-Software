606435f8ec7e947cddaf3345d4a724bfc2088efd
Brett Chiu <72076153+brettc-git@users.noreply.github.com>
2024-11-30 12:01:41 -0800

    Update passwordGenerator.cpp

--- passwordGenerator.cpp
+++ passwordGenerator.cpp
@@ -6,26 +6,36 @@
 #include <random>
 
 std::string passwordGenerator(int length) {
-	const std::string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+={[}]|:;\"'<,>.?/\\";
+
+	if (length < 5) {
+		return "ERROR";
+	}
+	// All possible characters for a pssword 
+
+	const std::string CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+={[}]|:;\"'<,>.?/\\";
+
+	// Random generator 
 	std::random_device random;
 	std::mt19937 generator(random());
-	std::uniform_int_distribution<> distribution(0, chars.size() - 1);
+	std::uniform_int_distribution<> distribution(0, CHARS.size() - 1);
+
 
 	std::string pass;
 
 	for (int i = 0; i < length; ++i) {
-	
-		pass += chars[distribution(generator)];
+		// Append characters from the distribution of chars to make the password	
+		pass += CHARS[distribution(generator)];
 	}
-
 	return pass;
 }
 
 int main() {
 	srand(time(0));
 	std::cout << "Entering a length for password: " << std::endl;
-	std::string first_p = passwordGenerator(14);
-	std::cout << first_p;
-
+	std::string first_p = passwordGenerator(2);
+	if (first_p != "ERROR")
+		std::cout << first_p;
+	else
+		std::cout << "Password too short!";
 	return 0;
 }
