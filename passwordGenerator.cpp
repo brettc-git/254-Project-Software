b7307650c433f5e8abcaf1947b4f0af4ea6c092d
Brett Chiu <72076153+brettc-git@users.noreply.github.com>
2024-11-30 11:46:32 -0800

    Update passwordGenerator.cpp

--- passwordGenerator.cpp
+++ passwordGenerator.cpp
@@ -3,19 +3,29 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <random>
 
 std::string passwordGenerator(int length) {
-	std::string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	std::string numeric = "0123456789";
-	std::string special = "~`!@#$%^&*()_-+={[}]|:;\"'<,>.?/\\";
+	const std::string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+={[}]|:;\"'<,>.?/\\";
+	std::random_device random;
+	std::mt19937 generator(random());
+	std::uniform_int_distribution<> distribution(0, chars.size() - 1);
+
 	std::string pass;
 
+	for (int i = 0; i < length; ++i) {
+	
+		pass += chars[distribution(generator)];
+	}
+
+	return pass;
 }
 
 int main() {
 	srand(time(0));
 	std::cout << "Entering a length for password: " << std::endl;
 	std::string first_p = passwordGenerator(14);
+	std::cout << first_p;
 
 	return 0;
 }
