88edca26b14f915ff560883ac89d24f736f93843
Brett Chiu <72076153+brettc-git@users.noreply.github.com>
2024-11-30 19:09:55 -0800

    Update passwordGenerator.cpp

--- passwordGenerator.cpp
+++ passwordGenerator.cpp
@@ -1,109 +1,81 @@
 // Implement Somewhere else Later
 
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <random>
-#include <string>
-#include <fstream>
+#include <wx/wx.h>
 
-// Checks if our password has a special character, returns true if a single one is found, false if there are none 
-bool containsSpecial(const std::string our_string) {
-	for (char c : our_string) {
-		if (!std::isalnum(c))
-			return true;
-	}
-	return false;
+class App : public wxApp {
+public:
+	virtual bool OnInit();
+};
+
+class Frame : public wxFrame {
+public:
+	Frame(const wxString& title);
+private:
+	
+	void OnAbout(wxCommandEvent& event);
+	void OnExit(wxCommandEvent& event);
+};
+
+bool App::OnInit()
+{
+	Frame* window = new Frame("Password Strength Checker");
+	window->Show(true);
+	return true;
 }
 
-bool containsUpper(const std::string our_string) {
-	for (char c : our_string) {
-		if (!std::isupper(c))
-			return true;
-	}
-	return false;
+// Password Generator Length
+// auto dialog = wxNumberEntryDialog {this, "", "Password Length", "Generate Passwords"
+
+
+Frame::Frame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title)
+{
+
+	wxPanel* panel = new wxPanel(this, wxID_ANY);
+	wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
+
+	// 
+	wxTextCtrl* textbox = new wxTextCtrl(panel, wxID_ANY, "Enter password...");
+	// Checkbox to show password 
+	wxCheckBox* showPasswordCheckbox = new wxCheckBox(panel, wxID_ANY, "Show Password");
+
+	sizer->Add(textbox, 0, wxALIGN_CENTER_HORIZONTAL | wxALIGN_CENTER_VERTICAL);
+	panel->SetSizer(sizer);
+	sizer->Fit(this);
+
+	wxMenuBar* menuBar = new wxMenuBar;
+
+	wxMenu* menu = new wxMenu;
+
+	menuBar->Append(menu, "&File");
+
+
+	menu->AppendSeparator();
+	menu->Append(wxID_EXIT);
+	// Menu bar implementation 
+
+
+	wxMenu* menuEdit = new wxMenu;
+
+	wxMenu* menuHelp = new wxMenu;
+
+	menuBar->Append(menuEdit, "&Edit");
+	menuBar->Append(menuHelp, "&Help");
+
+	SetMenuBar(menuBar);
+	Bind(wxEVT_MENU, &Frame::OnExit, this, wxID_EXIT);
 }
 
-bool containsNumber(const std::string our_string) {
-
-	for (char c : our_string) {
-		if (!std::isdigit(c))
-			return true;
-	}
-	return false;
+void Frame::OnAbout(wxCommandEvent& event) {
+	wxMessageBox("Team Members: Brett Chiu, Rene Acosta, Cristian Victorio");
 }
 
-bool containsLower(const std::string our_string) {
-	for (char c : our_string) {
-		if (!std::islower(c))
-			return true;
-	}
-	return false;
-}
-std::string passwordGenerator(int length) {
-
-	if (length < 8) {
-		return "ERROR";
-	}
-	// All possible characters for a pssword 
-
-	// Characters < and > are excluded since they can cause problems for web browsers
-	const std::string CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+={[}]|:;\"',.?/\\";
-
-	// Random generator 
-	std::random_device random;
-	std::mt19937 generator(random());
-	std::uniform_int_distribution<> distribution(0, CHARS.size() - 1);
-
-	// Our password variable
-
-	std::string pass;
-	do {
-		// Initialize password with empty, will be empty again if password doesn't contain special character
-		pass = "";
-		for (int i = 0; i < length; ++i) {
-			// Append characters from the distribution of chars to make the password	
-			pass += CHARS[distribution(generator)];
-		}
-
-		// Our password in this generator should contain at least one special character
-		/*if containsSpecial(pass) == false*/
-	} while (containsSpecial(pass) == false || containsUpper(pass) == false || containsNumber(pass) == false || containsLower(pass) == false);
-
-	// Return when at least once special character is found in password
-	return pass;
+// Exit function 
+void Frame::OnExit(wxCommandEvent& event) {
+	Close(true);
 }
 
+// void function that happens when file is done loading passwords
+// wxMessageBox("Passwords successfully generated.", wxOK | wxICON_INFORMATION)l;
+wxIMPLEMENT_APP(App);
 
 
-int main() {
-	srand(time(0));
-
-	std::ofstream Myfile("ourpasswords.txt");
-
-	if (!Myfile.is_open()) {
-		std::cerr << "File not found. Error.";
-		return 1;
-	}
-
-	std::string line; 
-	int num_of_pwds = 14;
-	int batch_length = 20;
-	std::cout << "Specifying how many passwords: " << num_of_pwds << std::endl;
-	std::cout << "of length: " << batch_length << std::endl;
-
-	for (size_t i = 0; i < num_of_pwds; i++) {
-		Myfile << passwordGenerator(batch_length) << std::endl;
-	}
-
-	std::cout << "All " << num_of_pwds << " passwords successfully generated.";
-	Myfile.close();
-
-	/*std::cout << "Entering a length for password: " << std::endl;
-	std::string first_p = passwordGenerator(23);
-	if (first_p != "ERROR")
-		std::cout << first_p;
-	else
-		std::cout << "Password too short!";*/
-	return 0;
-}
