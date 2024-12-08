ebe8723708f294a79428327d8479d6cf9179dac7
Brett Chiu <72076153+brettc-git@users.noreply.github.com>
2024-12-08 13:33:48 -0800

    Update password.h

--- password.h
+++ password.h
@@ -3,6 +3,9 @@
 
 #include <string>
 
+bool containsSpecial(const std::string our_string);
+bool containsUpper(const std::string our_string);
+bool containsNumber(const std::string our_string);
 std::string passwordGenerator(int length, bool upper, bool num, bool special);
 
 #endif
